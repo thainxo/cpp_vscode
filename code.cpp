494e352750dcb3b6ac83d55f06ca35d16e5bcdc4
thainxo <testgameoauth@gmail.com>
2022-10-05 18:02:39 +0700

    input

--- code.cpp
+++ code.cpp
@@ -1,79 +1,39 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <cstring>
+#include <bits/stdc++.h>
+using namespace std;
 
 #define MAX 200001
- 
-void swapArr(int arr[], int a, int b)
+int cw[52];
+int d[5001];
+
+void process(int N, int K, int cw[])
 {
- int tmp = arr[a];
- arr[a] = arr[b];
- arr[b] = tmp;
+        for (int i = 0; i <= K; i++) {
+                d[i] = 0;
+        }
+        d[0] = 1;
+        for (int i = 0; i < N; i++) {
+                for (int j = K; j >= cw[i]; j--) {
+                        if (d[j - cw[i]]) {
+                                d[j] = 1;
+                        }
+                }
+        }
+        cout << endl;
 }
- 
-void quickSort(int arr[], int start, int end)
-{
- if (end - start <= 1) return;
- 
- int pivot = arr[start];
- int i = start + 1;
- int j = end - 1;
- int mid = 0;
- 
- while (i < j) {
-  while ((i < j) && (arr[i] < pivot)) i += 1;
-  while ((j > i) && (arr[j] > pivot)) j -= 1;
-  if (i < j) {
-   swapArr(arr, i, j);
-   i += 1; j -= 1;
-  }
- }
- if (arr[i] < pivot) mid = i;
- else mid = i - 1;
- 
- swapArr(arr, start, mid);
- quickSort(arr, start, mid);
- quickSort(arr, mid + 1, end);
-}
- 
+
 int main()
 {
         int T;
-        scanf("%d", &T);
-        printf("hello a %d", T);
+        cin >> T;
+        for (int t = 0; t < T; t++) {
+                int N, K;
+                cin >> N >> K;
+                for (int i = 0; i < N; i++) {
+                        cin >> cw[i];
+                }
+
+                process(N, K, cw);
+        }
+
         return 0;
-//  int T;
- 
-//  scanf("%d", &T);
- 
-//  while (T > 0) {
-//   int N;
-//   int result = 1;
-//   int light_x[MAX];
-//   int light_y[MAX];
- 
-//   scanf("%d", &N);
- 
-//   memset(light_x, 0, sizeof(light_x));
-//   memset(light_y, 0, sizeof(light_y));
-   
-//   for (int i = 0; i < N; i++) {
-//    scanf("%d %d", &light_x[i], &light_y[i]);
-//   }
- 
-//   quickSort(light_x, 0, N);
-//   quickSort(light_y, 0, N);
- 
-//   int cnt_x = 0, cnt_y = 0;
-//   for (int i = 0; i < N; i++) if (((i + 1) == N) || (light_x[i] != light_x[i + 1])) cnt_x++;
-//   for (int i = 0; i < N; i++) if (((i + 1) == N) || (light_y[i] != light_y[i + 1])) cnt_y++;
- 
-//   int is_balanced = (cnt_x * cnt_y == N);
-//   if (is_balanced) printf("BALANCED\n");
-//   else printf("NOT BALANCED\n");
- 
-//   T -= 1;
-//  }
- 
- return 0;
 }
--- gen_input.cpp
+++ gen_input.cpp
@@ -1,26 +1,47 @@
-/* rand example: guess the number */
-#include <stdio.h>      /* printf, scanf, puts, NULL */
-#include <stdlib.h>     /* srand, rand */
-#include <time.h>       /* time */
-#include <iostream>
-#include <fstream>
+#include <bits/stdc++.h>
 using namespace std;
+
+int comparator(const void *p, const void *q) 
+{
+    int l = *((int *)p);
+    int r = *((int *)q);
+    return (l - r);
+}
 
 int main(int argc, char *argv[])
 {       
     /* initialize random seed: */
     srand (time(NULL));
 
-    cout << 1 << endl;
     // long long a = llabs((long long)rand() *(long long)rand()) % ((long long)1e9) + 1;
     // long long b = llabs((long long)rand() *(long long)rand()) % ((long long)1e9) + 1;
     // long long k = llabs((long long)rand() *(long long)rand()) % ((long long)2e9) + 1;
 
-    long long a = llabs((long long)rand()) % ((long long)512) + 1;
-    long long b = llabs((long long)rand()) % ((long long)512) + 1;
-    long long k = llabs((long long)rand()) % ((long long)1024) + 1;
-    
-    cout << a << " " << b << " " << k << endl;
-    cout << endl;
+    long long N = llabs(rand() % 50) + 1;
+    long long K = llabs(rand() % 5000) + 1;
+    int cw[N];
+    memset(cw, 0, sizeof(cw));
+    int count = 0;
+    while (count < N) {
+        long long w = llabs(rand() % 5000) + 1;
+        int exist = 0;
+        for (int i = 0; i < N; i++) {
+            if (cw[i] == w) {
+                exist = 1;
+                break;
+            }
+        }
+        if (exist == 0) {
+            cw[count] = w;
+            count++;
+        }
+    }
+
+    cout << 1 << endl;
+    cout << N << " " << K << endl;
+    qsort(cw, N, sizeof(int), comparator);
+    for (int i = 0; i < N; i++) {
+        cout << cw[i] << " ";
+    }
     return 0;
 }
