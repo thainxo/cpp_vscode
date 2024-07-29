24e56de3895f70c74edd740a04c12c131320e4aa
thainxo <46858614+thainxo@users.noreply.github.com>
2024-07-29 16:49:21 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,37 @@
-#!/bin/bash
-/usr/bin/gdb
+#!/usr/bin/env bash
+
+help() {
+    echo "1. compile and run with input from terminal"
+    echo "2. compile and run with input from input.txt"
+    echo "3. gen input and compare with base"
+    echo "4. gen input and check timeout"
+}
+
+main() {
+    local opt="$1"
+    case "$opt" in
+        "1")
+            echo "1. compile and run with input from terminal"
+            g++ code.cpp  -o build/run
+            build/run
+        ;;
+        "2")
+            echo "2. compile and run with input from input.txt"
+            g++ code.cpp  -o build/run
+            cat input.txt | build/run > output.txt
+            diff output.txt origin.txt
+        ;;
+        "3")
+            echo "3. gen input and compare with base"
+            ./cmp.sh
+        ;;
+        "4")
+            echo "3. gen input and compare with base"
+            ./wl.sh
+        ;;
+        *)
+            help
+        ;;
+    esac
+}
+main $@
